4406cbf6505c55941c1d40ea761946f96441e110
NELLAIAPPAN S <nellaiappan264426@gmail.com>
2022-07-15 13:13:12 +0530

    basics

---
+++ basics.py
@@ -0,0 +1,47 @@
+
+
+no ='nellai'
+print(no)
+College = ('IITM COLLEGE')
+print(College)
+college_city='chennai'
+print(college_city)
+<html>
+<head>
+<title>
+Learn_python)
+</title>
+</head>
+<body>
+<h1>IITM COLLEGE CHENNAI</h1>
+</html>
+#fundamental datatype 5
+#all are immutable(memory section)
+
+
+#complicated datatype
+#bytes;group of byte numbers(value)
+values=[100,90,99,90,98]
+#      0 ,1 ,2 ,3 ,4
+#     -5,-4,-3,-2,-1
+print(values)
+values=bytes(values)
+total=0
+for add in values: #tab
+    print(values)
+    total =total + add #indentation
+    print(total)
+print(type(values))
+#input datatype (save only str)
+no=int(input('enter any value'))
+print(no%2)
+if no%2==0:
+    print('Even Number')
+else:
+    print('odd number')
+
+number= 0,2
+print(number)
+no=float(554)
+print(float(no))
+
