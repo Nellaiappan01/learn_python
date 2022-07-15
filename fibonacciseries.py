5c7edd8954d39350a6f7dd9199c9afcb7a43f76b
NELLAIAPPAN S <nellaiappan264426@gmail.com>
2022-07-15 13:33:35 +0530

    typesfibonacciseries

--- fibonacciseries.py
+++ fibonacciseries.py
@@ -1,19 +1,33 @@
 #   first  second   third
+#    -1      0       1
 #     0      1       1
 #     1      1       2
 #     1      2       3
 #     2      3       5
 
-first = 0
+#type-1
+first = -1
 second = 1
 count = 1
-#print(first)
-#print(second)
-while count<=2:
+#while count<=10:
+while True:
     third=first+second
     print(third)
-    first=second
-    second=third
-    count+=1
-else:
+    if third==34:
+        break
+    first = second
+    second = third
+    count+= 1
+#type-2
+first = -1
+second = 1
+count = 1
+
+while count<=10:
+    third=first+second
     print(third)
+    first = second
+    second = third
+    count+= 1
+    
+#type-3
