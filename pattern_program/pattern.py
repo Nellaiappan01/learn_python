3a02271e5009cbbbb7dc9efb1f1a41ee962445ed
NELLAIAPPAN S <nellaiappan264426@gmail.com>
2022-07-16 11:57:15 +0530

    pattern_loop

---
+++ pattern_program/pattern.py
@@ -0,0 +1,13 @@
+#   * * * * *
+count="0"
+for i in range(5):
+    print("*",end='')
+print()
+
+#many "str method"
+count="0"
+for i in range(11):#row ranges
+    print("* "*10)#multiples
+   
+print()
+
