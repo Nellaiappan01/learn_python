941736e57e1f08a6e18d71a9078c4d1d0713a2bf
NELLAIAPPAN S <nellaiappan264426@gmail.com>
2022-07-22 22:31:51 +0530

    recal

--- for loop/alpha.py
+++ for loop/alpha.py
@@ -14,11 +14,11 @@ for no in range(10,1,-2):
     print(no,end=' ')
 print()
 #reversed alpha type-1
-word='nellai'
+word='nellaiappan'
 for i in range(len(word)-1,-1,-1):
     print(word[i],end=" ")
 print()
 #reversed alpha type-2 palindrome
-word='nellai'
+word='nellaiappan'
 word2=word[ : : -1]
 print(word2,end= "       ")
