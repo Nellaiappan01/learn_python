4635bd69f82763005f22b6cdc1a440858f0d869e
NELLAIAPPAN S <nellaiappan264426@gmail.com>
2022-07-26 20:43:09 +0530

    coursera

--- coursera/quiz01.py
+++ coursera/quiz01.py
@@ -17,3 +17,5 @@ for i in range(10):
   friends = ['Taylor', 'Alex', 'Pat', 'Eli']
 for friend in friends:
     print("Hi " + friend)
+#Use Python to calculate (((1+2)*3)/4)5
+print((((1+2)*3)/4)**5)
