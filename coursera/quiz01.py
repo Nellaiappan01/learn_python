3b7bef614be78c9dc34c7d176a7936dbb15ef6e3
NELLAIAPPAN S <nellaiappan264426@gmail.com>
2022-07-26 20:39:21 +0530

    coursera

---
+++ coursera/quiz01.py
@@ -0,0 +1,8 @@
+#Output a message that says “Programming in Python is fun!” to the screen.
+print("Programming in Python is fun!")
+#Replace the _ placeholder and calculate the Golden ratio: $\frac{1+\sqrt{5}}{2}$
+ratio = (1 + 5**.5) / 2
+print(ratio)
+#Convert this Bash command into Python:
+#             echo Have a nice day
+print('Have a nice day')
