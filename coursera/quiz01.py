3559b00b7a02d6e5a754dfd673399d07512d1022
NELLAIAPPAN S <nellaiappan264426@gmail.com>
2022-07-26 20:42:19 +0530

    coursera

--- coursera/quiz01.py
+++ coursera/quiz01.py
@@ -9,3 +9,11 @@ print('Have a nice day')
 #Fill in the correct Python commands to put “This is fun!” onto the screen 5 times.
 for i in range(5):
   print("This is fun!")
+  #Select the Python code snippet that corresponds to the following Javascript snippet:
+                                      #for (let i = 0; i < 10; i++)console.log(i);}
+for i in range(10):
+  print(i)
+
+  friends = ['Taylor', 'Alex', 'Pat', 'Eli']
+for friend in friends:
+    print("Hi " + friend)
