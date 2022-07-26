88ebaba626651d0836f2d8c6510e3bd07b63d8ce
NELLAIAPPAN S <nellaiappan264426@gmail.com>
2022-07-26 20:40:07 +0530

    coursera

--- coursera/quiz01.py
+++ coursera/quiz01.py
@@ -6,3 +6,6 @@ print(ratio)
 #Convert this Bash command into Python:
 #             echo Have a nice day
 print('Have a nice day')
+#Fill in the correct Python commands to put “This is fun!” onto the screen 5 times.
+for i in range(5):
+  print("This is fun!")
